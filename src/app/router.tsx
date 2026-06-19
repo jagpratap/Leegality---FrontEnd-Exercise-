185301e0f2fd064ee98e80a0ff697302abecf58e
Jagpratap <jagpratapchauhan@gmail.com>
2026-06-19 10:35:23 +0530

    Add GitHub Pages deployment

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,4 +2,6 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { appRoutes } from './routes'
 
-export const router = createBrowserRouter(appRoutes)
+export const router = createBrowserRouter(appRoutes, {
+    basename: '/Leegality---FrontEnd-Exercise-/',
+})
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './app/App.tsx'
 
+const redirectPath = new URLSearchParams(window.location.search).get('redirect')
+
+if (redirectPath) {
+  window.history.replaceState(null, '', redirectPath)
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App/>
